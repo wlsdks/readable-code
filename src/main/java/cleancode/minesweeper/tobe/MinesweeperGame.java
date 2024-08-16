7feb25d34f728cf7e87c66f4b48ab85dfe991f4d
wlsdks <wlsdks12@naver.com>
2024-08-16 11:52:58 +0900

    :sparkles: 커스텀 예외코드 추가

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -3,7 +3,6 @@ package cleancode.minesweeper.tobe;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class MinesweeperGame {
 
@@ -48,8 +47,10 @@ public class MinesweeperGame {
                 String cellInput = getCellInputFromUser();
                 String userActionInput = getUserActionInputFromUser();
                 actOnCell(cellInput, userActionInput);
-            } catch (IllegalArgumentException e) {
+            } catch (AppException e) {
                 System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("프로그램에 문제가 생겼습니다.");
             }
         }
     }
@@ -79,7 +80,7 @@ public class MinesweeperGame {
             return;
         }
 
-        System.out.println("잘못된 번호를 선택하셨습니다.");
+        throw new AppException("잘못된 번호를 선택하셨습니다.");
     }
 
     private static void changeGameStatusToLose() {
@@ -146,7 +147,7 @@ public class MinesweeperGame {
     private static int convertRowFrom(char cellInputRow) {
         int rowIndex = Character.getNumericValue(cellInputRow) - 1;
         if (rowIndex >= BOARD_ROW_SIZE){
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new AppException("잘못된 입력입니다.");
         }
 
         return rowIndex;
@@ -164,7 +165,7 @@ public class MinesweeperGame {
             case 'h' -> 7;
             case 'i' -> 8;
             case 'j' -> 9;
-            default -> throw new IllegalArgumentException("잘못된 입력입니다.");
+            default -> throw new AppException("잘못된 입력입니다.");
         };
     }
 
