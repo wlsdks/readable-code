3aa91508beb69f014b21ce666c88f21dad8ffe88
wlsdks <wlsdks12@naver.com>
2024-08-17 22:52:06 +0900

    :recycle: DIP 원칙을 적용

---
+++ src/main/java/cleancode/minesweeper/tobe/io/OutputHandler.java
@@ -0,0 +1,24 @@
+package cleancode.minesweeper.tobe.io;
+
+import cleancode.minesweeper.tobe.AppException;
+import cleancode.minesweeper.tobe.GameBoard;
+
+public interface OutputHandler {
+
+    void showGameStartComments();
+
+    void showBoard(GameBoard board);
+
+    void showGameWinningComment();
+
+    void showGameLosingComment();
+
+    void showCommentForSelectingCell();
+
+    void showCommentForUserAction();
+
+    void showExceptionMessage(AppException e);
+
+    void showSimpleMessage(String message);
+
+}
